fdf4d6e811a50ef6ceb5e39f998d5d6b2ab18f15
DavidAyalaDW <luis.zanabria2@unmsm.edu.pe>
2022-12-12 12:22:00 -0500

    add promiseall

--- pages/api/customers/[id].js
+++ pages/api/customers/[id].js
@@ -1,3 +1,35 @@
+async function requestCustomerInfo(id){
+
+  const request = await fetch(`https://www.kurufootwear.com/admin/api/2022-10/customers/${id}.json`,{
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Shopify-Access-Token': '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'
+    }
+  });
+
+  const response = await request.json();
+
+  return response
+
+}
+
+async function requestOrders(id){
+
+  const request = await fetch(`https://www.kurufootwear.com/admin/api/2022-10/orders.json?customer_id=${id}`,{
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Shopify-Access-Token': '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'
+    }
+  });
+
+  const response = await request.json();
+
+  return response
+
+}
+
 export default async function handler(req, res) {
   
   const {
@@ -9,18 +41,11 @@ export default async function handler(req, res) {
 
   try {
 
-    const request = await fetch(`https://www.kurufootwear.com/admin/api/2022-10/customers/${id}.json`,{
-      method: "GET",
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Shopify-Access-Token': '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'
-      }
-    });
+    const [customerData, ordersData] = await Promise.all([requestCustomerInfo(id),requestOrders(id)]);
+
+    const {id: userID , email, first_name, last_name} = customerData["customer"];
   
-    const response = await request.json();
-    const {id: userID , email, first_name, last_name} = response["customer"];
-  
-    res.status(200).json({customer_info: {id :userID ,email, first_name, last_name}})
+    res.status(200).json({customer_info: {id :userID ,email, first_name, last_name}, orders: ordersData})
 
   } catch (error) {
     res.status(200).json({error})
